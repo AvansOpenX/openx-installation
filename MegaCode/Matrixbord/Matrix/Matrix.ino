5c853e6e41fa8721a5ce4f2ac4bb93fac52aae88
avmarques <120378547+avmarques@users.noreply.github.com>
2023-01-10 14:57:50 +0100

    Show scores

--- MegaCode/Matrixbord/Matrix/Matrix.ino
+++ MegaCode/Matrixbord/Matrix/Matrix.ino
@@ -15,7 +15,11 @@
 //seriele communicatie
 #define Score A5
 
+//scores
 int highscore;
+int scoreP1;
+int scoreP2;
+
 
 RGBmatrixPanel matrix(A, B, C, D, CLK, LAT, OE, false, 64);
 
@@ -35,11 +39,11 @@ void setup() {
 
 void loop(){
 
-  showHighscore();
+  showScoreP2();
 
   delay(4000);
 
-  highscore = analogRead(Score);
+  //highscore = analogRead(Score);
 }
 
 
@@ -48,8 +52,46 @@ void showHighscore(){
   matrix.print("Highscore: ");
   //next line 
   matrix.setCursor(23, 18);
-  matrix.print(highscore);
+  //matrix.print(highscore);
   delay(4000);
+  matrix.clear();
+
+}
+
+//show score if one player played
+void showScoreP1(){
+
+}
+
+//show score if two players played
+void showScoreP2(){
+
+  int scoreP1 = 199;
+  int scoreP2 = 256;
+
+  matrix.setCursor(2, 6);    // (x, y)
+  matrix.print("Player1:");
+  matrix.print(scoreP1);
+
+  //next line 
+  matrix.setCursor(2, 18);
+  matrix.print("Player2:");
+  matrix.print(scoreP2);
+
+  delay(10000);
+  matrix.clear();
+
+  showWinner();
+
+}
+
+void showWinner(){
+
+  matrix.setTextSize(2);
+  matrix.setCursor(3, 12);
+  matrix.print("Player 2 is the winner");
+
+  delay(5000);
   matrix.clear();
 
 }
